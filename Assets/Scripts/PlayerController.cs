5526dab966937b118154533a60fc19ecbf1d2911
anquito <r-andg@hotmail.com>
2020-03-02 11:02:48 -0800

    Part 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     const float horizontalBoundary = 10.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,10 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
         if (transform.position.x > horizontalBoundary)
         {
             transform.position = new Vector3 (horizontalBoundary, transform.position.y, transform.position.z);
@@ -25,8 +30,9 @@ public class PlayerControl : MonoBehaviour
             transform.position = new Vector3(-horizontalBoundary, transform.position.y, transform.position.z);
         }
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
