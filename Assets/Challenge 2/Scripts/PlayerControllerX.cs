b133fa7d5cfb4e456a219a390a59f3c74e0ae8b6
anquito <r-andg@hotmail.com>
2020-03-11 11:28:20 -0700

    Unit 2 Challenge

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogRate = 0.5f;
+    private float dogDelay = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > dogDelay)
+        {
+            dogDelay = Time.time + dogRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private const float spawnLimitXLeft = -22.0f;
+    private const float spawnLimitXRight = 7.0f;
+    private float spawnPosY = 30.0f;
+
+    private float startDelay = 1.0f;
+    //private float spawnInterval = 4.0f;
+    private float spawnIntervalMin = 3.0f;
+    private float spawnIntervalMax = 5.0f;
+    private float randomInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomInterval = (Random.Range(spawnIntervalMin, spawnIntervalMax));
+        InvokeRepeating("SpawnRandomBall", startDelay, randomInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int randomBall = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[randomBall], spawnPos, ballPrefabs[randomBall].transform.rotation);
+    }
+
+}
